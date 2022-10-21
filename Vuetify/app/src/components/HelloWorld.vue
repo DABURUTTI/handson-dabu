881790ea03091e1b8a3f03926fcfb286d56a3947
DABU <daburutti@gmail.com>
2022-10-21 11:14:12 +0900

    Good

--- Vuetify/app/src/components/HelloWorld.vue
+++ Vuetify/app/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/cps_logo.png')"
           class="my-3"
           contain
           height="200"
@@ -11,9 +11,7 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to<br />IWAI-LAB HANDSON
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to IWAI-LAB</h1>
 
         <p class="subheading font-weight-regular">
           岩井研へようこそ！
@@ -28,6 +26,9 @@
 export default {
   name: "HelloWorld",
 
-  data: () => ({}),
+  data: () => ({
+    model: 0,
+    pics: ["download.jpeg", "access_01.jpeg"],
+  }),
 };
 </script>
